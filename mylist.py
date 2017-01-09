e263157a1603a644b5667d3ef2f3cc4b339475df
Sun42 <sundas.christophe@gmail.com>
2017-01-09 15:41:29 +0000

    first commit

---
+++ mylist.py
@@ -0,0 +1,76 @@
+class mylist:
+    """
+    Personal List class
+
+    Attributes:
+    length(int) Represents the number of elements in the list
+
+    """
+    def __init__(self, ):
+        """
+        """
+
+    def index(element):
+        """
+        """
+        pass
+
+    def count(element):
+        """
+        """
+        pass
+
+    def is_present(element):
+        """
+        """
+        pass
+
+    def append(element):
+        """
+        """
+        pass
+
+    def extend(element):
+        """
+        """
+        pass
+
+    def insert(index, element):
+        """
+        """
+        pass
+
+    def delete(index):
+        """
+        """
+        pass
+
+    def remove(element):
+        """
+        """
+        pass
+
+    def pop(element):
+        """
+        """
+        pass
+
+    def slice(pattern):
+        """
+        """
+        pass
+
+    def is_true():
+        """
+        """
+        pass
+
+    def print():
+        """
+        """
+        pass
+
+    def sort():
+        """
+        """
+        pass
